919bb6057f5b6aeb482cf22937347c663a487106
Mitesh Prakash <68989710+MITU16EE01@users.noreply.github.com>
2022-06-26 14:32:03 +0530

    Health_management_system
    # Health Management System
    #For Each client create two separate file one each for his diet and exercise with date and time at its beginning in a logbook(file)
    #if it exist then you need not to create another file each time update in the exiting file else do the above
    #try to catch all the error
    # Write a function that when executed takes as input the (activity{log, retrieve}),(client name), (file type{diet, exercise})

---
+++ Health_management_system.py
@@ -0,0 +1,71 @@
+# Health Management System
+#For Each client create two seperate file one each for his dit and exercise with date and time at its begining in a logbook(file)
+#if it exist then you need not to craete another file each time update in the exixting file else do the above
+#try to catch all the error
+# Write a function that when executed takes as input the (activity{log,retrive}),(client name), (file type{diet,exercise})
+
+def getdate():
+    import datetime
+    return  datetime.datetime.now()
+
+
+def HMS_L():
+    a = input("Enter the client name:")
+    b = input("Enter the diet-D or exercise-E:")
+    if b=="D":
+        z = "diet"
+    else:
+        z = "exercise"
+    try:
+        with open(f"{a}.{b}.txt","x") as f:
+            y  = input(f"Enter the {z} for {a}: ")
+            f.write(f"[{getdate()}] {y}\n")
+    except:
+        with open(f"{a}.{b}.txt","a") as f:
+            y = input(f"Enter the {z} for {a}: ")
+            f.write(f"[{getdate()}] {y}\n")
+
+
+def HMS_R():
+    a = input("Enter the client name:")
+    b = input("Enter the diet-D or exercise-E:")
+    if b=="D":
+        z = "diet"
+    else:
+        z = "exercise"
+    try:
+        with open(f"{a}.{b}.txt","r") as f:
+            print(f.read())
+    except:
+        print(f"No history found for {a}'s {z}. Please create a file first")
+
+
+def HMS(n):
+    n +=1
+    x = input("What do you want from HMS to:\n for Log Press {L}\n for retrive press {R}\n")
+    if x == "L":
+        HMS_L()
+        print("Data Has been entered Sucessfully")
+        rerun(n)
+    elif x =="R":
+        HMS_R()
+        print("Thanks for checking the data")
+        rerun(n)
+    else:
+        print("Check the input")
+        HMS()
+
+
+def rerun(n):
+    while(n<1):
+        HMS()
+        n+=1
+    else:
+        run  = input("Do you want to use HMS again?\n{y}--For yes\n{N}--For no\n")
+        if run == "Y":
+            HMS(n)
+        else:
+            print("Thanks for Using HMS")
+
+
+HMS(0)
